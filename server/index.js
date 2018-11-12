74d704ecf4205c6b79b12d9fee09fc89a593cb15
tonyOP1985 <toliverpaull@gmail.com>
2018-11-11 23:34:46 -0500

    Removed redundant folder and comment (fixes #10).

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
-// app.use(logger('tiny'));
+app.use(logger('tiny'));
 
 app.use('/api/gigs', gigs);
 
