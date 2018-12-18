a06170a2967f27b45ac30fb5216a8d1509d64c1d
tonyOP1985 <toliverpaull@gmail.com>
2018-12-18 00:30:32 -0500

    Created splash page.

--- client/src/views/Splash.vue
+++ client/src/views/Splash.vue
@@ -1,7 +1,27 @@
 <template>
-  <div>
-    Splash page at some point.
-  </div>
+  <v-content class="blue" fill-height>
+    <v-layout align-center justify-center fill-height column>
+      <h1 class="white--text banner text-xs-center">Gig Tracker</h1>
+      <div>
+        <v-btn
+            to="/register"
+            large
+            outline
+            round
+            color="white">
+          <b>Sign Up</b>
+        </v-btn>
+        <v-btn
+            to="/login"
+            large
+            outline
+            round
+            color="white">
+          <b>Sign In</b>
+        </v-btn>
+      </div>
+    </v-layout>
+  </v-content>
 </template>
 
 <script>
@@ -9,3 +29,11 @@ export default {
   name: 'splash'
 }
 </script>
+
+<style scoped>
+  .banner {
+    font-family: 'Roboto Condensed', sans-serif;
+    font-size: 3em;
+  }
+</style>
+
