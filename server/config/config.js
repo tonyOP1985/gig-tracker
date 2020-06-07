6f2e6bba4f1f89480b368e9ecc04864f7a46dcb9
Tony Oliver-Paull <toliverpaull@gmail.com>
2020-06-07 13:34:03 -0400

    Created config.js .

---
+++ server/config/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  development: {
+    username: 'postgres',
+    password: 'JackBruc3',
+    database: 'gigtracker',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  production: {
+    username: 'gigtracker',
+    password: 'gigtracker1',
+    database: 'gigtracker',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+};
